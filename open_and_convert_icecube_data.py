cb16a8a404a40bc6cf71a639c728febbcb4a4208
Daniel Smith <danielsmith@uchicago.edu>
2022-02-20 19:55:59 -0600

    More efficient way to load in icecube csv files.

--- open_and_convert_icecube_data.py
+++ open_and_convert_icecube_data.py
@@ -3,42 +3,36 @@ import matplotlib.pyplot as plt
 import scipy.interpolate
 import glob
 
-data_files = glob.glob("./data/IC*-events.txt")
-data_sigmas = []
-data_ra = []
-data_dec = []
-data_day = []
+
+data_files = glob.glob("./data/IC*.csv")
+
+data_day = np.array([])
+data_sigmas = np.array([])
+data_ra = np.array([])
+data_dec = np.array([])
 
 for data_file_name in data_files:
+    print("Loading filename: %s" % data_file_name)
     f = open(data_file_name)
 
-    for i, line in enumerate(f):
-        if(i == 0):
-            #print(line)
-            continue
-        
-        if(i % 10000 == 0):
-            print("Line", i,"of",data_file_name)
-        
-        line_ = line.split(" ")
-        line_ = np.array(line_)[np.array(line_) != '']
-        data_day += [float(line_[0])]
-        data_sigmas += [float(line_[2])]
-        data_ra += [float(line_[3])]
-        data_dec += [float(line_[4])]
+    data = np.loadtxt(data_file_name, dtype='float')
+    data_day = np.append(data_day, data[:, 0])
+    data_sigmas = np.append(data_sigmas, data[:, 2])
+    data_ra = np.append(data_ra, data[:, 3])
+    data_dec = np.append(data_dec, data[:, 4])
 
-#plt.hist(data_day, 1085)
-#plt.hist(data_day, 2000)
-#plt.show()
-#exit()
-        
-data_sigmas = np.deg2rad(np.array(data_sigmas))
-data_ra     = np.deg2rad(np.array(data_ra))
-data_dec    = np.deg2rad(np.array(data_dec))
-    
-data_sigmas = data_sigmas[np.abs(data_dec) < np.deg2rad(87.0)]
-data_ra     = data_ra[np.abs(data_dec) < np.deg2rad(87.0)]
-data_dec    = data_dec[np.abs(data_dec) < np.deg2rad(87.0)]
-data_sin_dec = np.sin(data_dec)
+data_sigmas = np.deg2rad(data_sigmas)
+data_ra = np.deg2rad(data_ra)
+data_dec = np.deg2rad(data_dec)
 
-np.savez("processed_data/output_icecube_data.npz", data_sigmas=data_sigmas, data_ra=data_ra, data_dec=data_dec, data_sin_dec=data_sin_dec)
+selection = np.abs(data_dec) < np.deg2rad(87.0)
+data_day = data_day[selection]
+data_sigmas = data_sigmas[selection]
+data_ra = data_ra[selection]
+data_dec = data_dec[selection]
+
+np.savez("processed_data/output_icecube_data.npz",
+         data_day=data_day,
+         data_sigmas=data_sigmas,
+         data_ra=data_ra,
+         data_dec=data_dec)
