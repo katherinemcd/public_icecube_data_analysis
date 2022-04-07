ce22690714ed3f4737952987525481e9a33b99da
Daniel Smith <danielsmith@uchicago.edu>
2022-04-06 22:44:26 -0500

    Analysis code to make all sky map much quicker to run.

---
+++ A01_open_and_convert_icecube_data.py
@@ -0,0 +1,30 @@
+import glob
+import numpy as np
+
+data_files = glob.glob("./data/icecube_10year_ps/events/IC*.csv")
+data_file_year = np.array([])
+data_day = np.array([])
+data_sigmas = np.array([])
+data_ra = np.array([])
+data_dec = np.array([])
+
+for data_file_name in data_files:
+    print("Loading filename: %s" % data_file_name)
+    f = open(data_file_name)
+
+    year = int(2011)#data_file_name.split("-")[-2])
+
+    data = np.loadtxt(data_file_name, dtype='float')
+    data_file_year = np.append(data_file_year,
+                               year * np.ones(len(data), dtype='int'))                                              
+    data_day = np.append(data_day, data[:, 0])
+    data_sigmas = np.append(data_sigmas, data[:, 2])
+    data_ra = np.append(data_ra, data[:, 3])
+    data_dec = np.append(data_dec, data[:, 4])
+
+np.savez("processed_data/output_icecube_data.npz",
+         data_day=data_day,
+         data_sigmas=data_sigmas,
+         data_ra=data_ra,
+         data_dec=data_dec,
+         data_file_year=data_file_year)
